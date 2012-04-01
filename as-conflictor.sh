550d59e9cd2fc3040ec41f6058d69b779d82c8e4
Adrian Smith <smith.adriane@gmail.com>
2012-04-01 16:54:47 -0700

    use random strings for as-conflictor

--- as-conflictor.sh
+++ as-conflictor.sh
@@ -2,12 +2,12 @@ set -x
 
 cd ../gimple-test2
 git pull
-echo flabba > README
+echo ${RANDOM}  > README
 git add -u
 git commit -m "makin conflicts"
 git push
 cd ../gimple-test
-echo fdjkfdjk > README
+echo ${RANDOM}  > README
 git add -u
 git commit -m "conflictzzz"
 
